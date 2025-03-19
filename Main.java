3fbce151ee37cc652f9fca35c2faada2989c2cfa
Carolina Segmiller <66015838+CarolinaSegmiller@users.noreply.github.com>
2025-03-19 10:27:47 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 package controle;
 
+
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        ControlaAluno controle = new ControlaAluno();
+
+        controle.exibirMenu();
+
+    }
 
 }
